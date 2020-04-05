2a1676be2423f37fa783a5b513a4306d73ffcf07
wanghsuan <wanghsuanqut@gmail.com>
2020-04-05 14:51:22 +1000

    product out of stock

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,31 +1,36 @@
 import React from 'react';
+import {formatPrice} from '../commons/helper'
 
-class Product extends React.Component{
 
-    product = {}
+function Product(props){
 
-    render(){
+    const{name,tags,image,price,status} = props.product;
+    const _pClass ={
+        available:'product',
+        unavailable:'product out-stock'
+    }
         return(
-            <div className='product'>
+            <div className={_pClass[status]}>
                     <div className='p-content'>
                     <div className='img-wrapper'>
+                        <div className="out-stock-text">Out OF Stock</div>
                         <figure className='image is4by3'>
-                            <img src='image/1.jpg' alt='shoes' />
+                            <img src={image} alt='shoes' />
                         </figure>
                     </div>
-                    <p className='p-tags'>25 Colors</p>
-                    <p className='p-name'>lorem</p>
                 </div>
+                <p className='p-tags'>{tags}</p>
+                <p className='p-name'>{name}</p>
              <div className='p-footer'>
-                <p className='price'>$688.00</p>
-                <button className='add-cart'>
+                <p className='price'>{formatPrice(price)}</p>
+                <button className='add-cart' disabled={status === 'unavailable'}>
                     <i className='fa fa-shopping-cart'></i>
                     <i className='fa fa-exclamation'></i>
                 </button>
              </div>
             </div>
         )
-    }
+  
 }
 
 export default Product;
--- src/component/Products.js
+++ src/component/Products.js
@@ -2,35 +2,48 @@ import React from 'react';
 import Product from './Product';
 import ToolBox from './ToolBox';
 
+const products = [
+    {
+        id:1,
+        name:'qut',
+        tags:'blue colors',
+        image:'image/1.jpg',
+        price:'80',
+        status:'available'
+    },
+    {
+        id:2,
+        name:'qut',
+        tags:'blue colors',
+        image:'image/2.jpg',
+        price:'80',
+        status:'unavailable'
+    },
+    {
+        id:3,
+        name:'qut',
+        tags:'blue colors',
+        image:'image/3.jpg',
+        price:'80',
+        status:'available'
+    }
+]
 
 export default function Products(){
+    
     return (
         <div>
         <ToolBox/>
             <div className='products'>
                 <div className='columns is-multiline is-desktop'>
-                    <div className='column is-3'>
-                     <Product/>
-                    </div>
-
-                    <div className='column is-3'>
-                    <Product/>
-                   </div>
-
-                   <div className='column is-3'>
-                   <Product/>
-                  </div>
-
-                  <div className='column is-3'>
-                  <Product/>
-                 </div>
-
-                 <div className='column is-3'>
-                 <Product/>
-                </div>
-                </div>
-               
-                
+                   
+                    {products.map(product => {
+                        return (    <div className='column is-3' key={product.id}>
+                                         <Product product={product}/>
+                                    </div>
+                                )
+                    })}
+                </div>     
             </div>   
         </div>   
       )
